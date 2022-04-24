6fb8d918a36c7cf188a9a22c922d92f2c07d724d
Vladimir Patsenko <v@patsenko.ru>
2022-04-24 09:43:00 +0300

    account

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,19 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-/**
- * https://vitejs.dev/config/
- * @type { import('vite').UserConfig }
- */
-export default {
-  plugins: [reactRefresh()],
-  server: {
-    host: '0.0.0.0',
-    hmr: {
-      port: 443,
-    }
-  }
-}
+
+// export default {
+//   plugins: [reactRefresh()],
+//   server: {
+//     open: "./index.html",
+//     host: 'localhost',
+//     hmr: {
+//       port: 3000,
+//     }
+//   }
+// }
+
+export default defineConfig({
+  plugins: [react()]
+})
